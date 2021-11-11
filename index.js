3486021557ae143254cff7de46e182041c3ad2c4
FE1979 <ronin911@gmail.com>
2021-11-11 17:07:12 +0200

    Connect Firebase

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5LkOnzYbIGtNVJysjad4FE-P8vuEBdTo",
+    authDomain: "my-projects-1f367.firebaseapp.com",
+    projectId: "my-projects-1f367",
+    storageBucket: "my-projects-1f367.appspot.com",
+    messagingSenderId: "543781647796",
+    appId: "1:543781647796:web:ea69f05e1676bd9b3b2ee1",
+    measurementId: "G-MKBGXRG65K"
+  };
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 setMinMaxBirthdate();
 
 function setMinMaxBirthdate() {
